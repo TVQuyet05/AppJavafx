c110760ef6cffc4edc707d30a786cf5d157729a5
TVQuyet05 <tranvanquyet09112005@gmail.com>
2024-11-22 01:59:48 +0700

    complete sign up and view book

--- src/main/java/org/example/librarymanager/Service/LibraryDatabase.java
+++ src/main/java/org/example/librarymanager/Service/LibraryDatabase.java
@@ -9,6 +9,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 // Class use Singleton pattern
 // Code all function to access database in here
 
@@ -31,7 +32,7 @@ public class LibraryDatabase {
     }
 
     public static LibraryDatabase getInstance() {
-        if (instance == null) {
+        if(instance == null) {
             instance = new LibraryDatabase();
         }
         return instance;
